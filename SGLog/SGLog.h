e096e3b195af753673a90b4f5f30148457ab4b9e
Daniel Loughney <Daniel.Loughney@russellreynolds.com>
2016-07-01 08:01:56 -0400

    Initial Commit

---
+++ SGLog/SGLog.h
@@ -0,0 +1,19 @@
+//
+//  SGLog.h
+//  SGLog
+//
+//  Created by Daniel Loughney on 7/1/16.
+//  Copyright © 2016 SpookyGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SGLog.
+FOUNDATION_EXPORT double SGLogVersionNumber;
+
+//! Project version string for SGLog.
+FOUNDATION_EXPORT const unsigned char SGLogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SGLog/PublicHeader.h>
+
+
